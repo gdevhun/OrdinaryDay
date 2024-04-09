51bb76b9ce9db4c801c8c33b332b25e58c1774b9
gdevhun <wjh9330@naver.com>
2024-04-09 13:13:20 +0900

    updated:트리거코드 비동기로 변경

--- Assets/QuestData.cs
+++ Assets/QuestData.cs
@@ -6,7 +6,7 @@ public class QuestData : ScriptableObject
 {
 	//퀘스트데이타 스크립터블오브젯트
 	public string questInfo;  //퀘스트정보
-	public int questNumber;  //퀘스트 순번
+	public string questName;  //퀘스트 이름
 	public List<string> talkText;
 	//public GameObject triggerObject; 트리거오브젝트는 런타임에서만 할당.
 }
--- Assets/Scripts/ETC/QuestTrigger.cs
+++ Assets/Scripts/ETC/QuestTrigger.cs
@@ -1,47 +1,55 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 public class QuestTrigger : MonoBehaviour
 {
     [SerializeField] private FirstPlayer FirstPlayer;
     public ScriptableObject scriptableObject;
-   
+    private bool _isNextText;
     private void Start()
     {
-        string totalName = scriptableObject.ToString();
-        QuestManager.Instance.LoadQuestData(totalName);
-        Debug.Log(QuestManager.Instance.questData.questNumber);
+        Debug.Log(scriptableObject.ToString());
+        QuestManager.Instance.LoadQuestData("QuestData1_1");
         // 처음에는 모두 비활성화
         //활성화되면 -> start에서 퀘스트정보 얻어오고
 
     }
 
-    private void OnTriggerEnter(Collider other)
+    /*private async UniTaskVoid DisplayNextText()
     {
-        if (other.CompareTag("Player"))
-        {
-            var textNum = QuestManager.Instance.totalTextCnt;
-            FirstPlayer.isFade = true;
-            while (textNum != 0)
-            {
-                TextManager.Instance.DisplayTextSlowly(QuestManager.Instance.questData.talkText[textNum]);
-                if (Input.GetKeyUp(KeyCode.Return) && !TextManager.Instance.isOverTextRoutine)
-                {   // 엔터, 텍스 출력코루틴이 끝나지않았다면
-                    TextManager.Instance.DisplayTextInstantly(); //함수호출로 텍스트 바로 렌더링
-                }
-                else if (Input.GetKeyUp(KeyCode.Return) && TextManager.Instance.isOverTextRoutine)
-                {   //엔터, 텍스 출력 코루틴이 끝난 상황이면
-                    TextManager.Instance.isOverTextRoutine = false;
-                    TextManager.Instance.talkTextUI.SetActive(false);  // 비활성화
-                }
-                textNum--;
-            }
-            FirstPlayer.isFade = false;
-            gameObject.SetActive(false);
+        await UniTask.Delay(TimeSpan.FromSeconds(0.5));
+        _isNextText = true;
+        await UniTask.Delay(TimeSpan.FromSeconds(1));
+        _isNextText = false;
+    }*/
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return) && !TextManager.Instance.isOverTextRoutine)
+        {   // 엔터, 텍스 출력코루틴이 끝나지않았다면
+            TextManager.Instance.DisplayTextInstantly(); //함수호출로 텍스트 바로 렌더링
+            //isOverTextRoutine true됨.
         }
     }
 
-    
+     private async UniTaskVoid OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            FirstPlayer.isFade = true;
+            for (int textNum = 0; textNum < QuestManager.Instance.totalTextCnt; textNum++)
+            {
+                TextManager.Instance.DisplayTextSlowly(QuestManager.Instance.questData.talkText[textNum]);
+                await UniTask.WaitUntil(() => Input.GetKeyUp(KeyCode.Return) && TextManager.Instance.isOverTextRoutine);
+            }
+            if(Input.GetKeyUp(KeyCode.Return))
+            {
+                TextManager.Instance.isOverTextRoutine = false;
+                TextManager.Instance.talkTextUI.SetActive(false);  // 비활성화
+                FirstPlayer.isFade = false;
+                gameObject.SetActive(false);
+            }
+        }
+    }
 }
