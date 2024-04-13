74d28224ce1ee7bdc697d62272cd04765a7a6e40
LeeJungHwi <101587101+LeeJungHwi@users.noreply.github.com>
2024-04-13 18:40:40 +0900

    FEAT : 핸드폰 상호작용

--- Assets/Scripts/ETC/Phone.cs
+++ Assets/Scripts/ETC/Phone.cs
@@ -4,18 +4,33 @@ using UnityEngine;
 
 public class Phone : InteractionBase
 {
-    public GameObject phone; //상호작용 폰
+    public GameObject phone; // 핸드폰
+
+    // 플레이어가 근처에 있음
     public override void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            SoundManager.Instance.SFXPlay(SfxType.PhoneFirstBell);
             isNear = true;
             interactionText.text = "E키로 핸드폰을 볼 수 있다.";
         }
     }
+
+    // 상호작용
     protected override void Interaction()
     {
+        if (!isInter) LookPhone(); // 핸드폰 보기
+    }
+
+    // 핸드폰 보기
+    private void LookPhone()
+    {
         phone.gameObject.SetActive(true);
+
+        isInter = true;
+
+        interactionText.text = "";
+
+        SoundManager.Instance.SFXPlay(SfxType.PhoneFirstBell);
     }
 }
