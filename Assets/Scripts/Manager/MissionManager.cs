c43f9f83bbe30f3d959ced33da53418ee6d486cd
gdevhun <wjh9330@naver.com>
2024-04-13 15:20:26 +0900

    Feat: 미션매니저추가

---
+++ Assets/Scripts/Manager/MissionManager.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MissionManager : Singleton<MissionManager>
+{
+    public List<string> missionTextList;
+    public GameObject missionTextPanel;
+    public TextMeshProUGUI missionText;
+
+    void Start()
+    {
+        missionTextPanel.SetActive(false);
+
+    }
+
+    public void InitMissonText(string missionTxt)
+    {
+        missionTextPanel.SetActive(true);
+        missionText.text = missionTxt;
+    }
+
+    public void HideMissionText()
+    {
+        missionTextPanel.SetActive(false);
+    }
+
+    public class enum Mission
+    {
+        
+    }
+}
+
