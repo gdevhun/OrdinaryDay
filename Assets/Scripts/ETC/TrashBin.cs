e04737c0382fce8cfe64828d405fd43e4370de1e
gdevhun <wjh9330@naver.com>
2024-04-14 16:56:57 +0900

    add 트래쉬빈 이벤트스크립트

---
+++ Assets/Scripts/ETC/TrashBin.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashBin : InteractionBase
+{
+    private string _curMissionState;
+    private int _totalTrashCleaned;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    protected override void Interaction()
+    {
+        
+    }
+}
