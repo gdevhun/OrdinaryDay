d4393fc449a193db50f757251611ae4364b722cd
gdevhun <wjh9330@naver.com>
2024-04-29 11:54:02 +0900

    Fix: 이벤트트리거연결

--- Assets/Scripts/ETC/Event/QuestTrigger.cs
+++ Assets/Scripts/ETC/Event/QuestTrigger.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 
 public class QuestTrigger : MonoBehaviour
 {
-    [SerializeField] private FirstPlayer firstPlayer;
+    private GameObject _firstPlayer;
     public ScriptableObject scriptableObject;
     public string scriptableObjectName;
     public GameObject nextTriggerObj;
@@ -17,18 +17,13 @@ public class QuestTrigger : MonoBehaviour
     private void OnEnable()
     {
         QuestManager.Instance.LoadQuestData(scriptableObjectName);
+        _firstPlayer = GameObject.FindGameObjectWithTag("Player");
         // 처음에는 모두 비활성화
         //활성화되면 -> start에서 퀘스트정보 얻어오고
     }
 
     private void Update()
     {
-       
-        /*if (Input.GetKeyUp(KeyCode.Return) && !TextManager.Instance.isOverTextRoutine)
-        {   // 엔터, 텍스 출력코루틴이 끝나지않았다면
-            TextManager.Instance.DisplayTextInstantly(); //함수호출로 텍스트 바로 렌더링
-            //isOverTextRoutine true됨.
-        }*/
 
         if (Input.GetKeyUp(KeyCode.Return) && TextManager.Instance.isOverTextRoutine)
         {
@@ -41,7 +36,7 @@ public class QuestTrigger : MonoBehaviour
             {
                 TextManager.Instance.isOverTextRoutine = false;
                 TextManager.Instance.talkTextUI.SetActive(false);  // 비활성화
-                firstPlayer.isFade = false;
+                _firstPlayer.gameObject.GetComponent<FirstPlayer>().isFade = false;
                 gameObject.SetActive(false);
                 nextTriggerObj.SetActive(true);
                 if (isPassWordEvent)
@@ -60,7 +55,7 @@ public class QuestTrigger : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            firstPlayer.isFade = true;
+            _firstPlayer.gameObject.GetComponent<FirstPlayer>().isFade = true;
             if (QuestManager.Instance.isLoadedData)
             {
                 for (int textNum = 0; textNum < QuestManager.Instance.totalTextCnt; textNum++)
