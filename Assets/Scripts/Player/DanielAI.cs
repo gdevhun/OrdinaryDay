280cd465bc8e1c2c13e828ff0cd1a469d8199272
LeeJungHwi <101587101+LeeJungHwi@users.noreply.github.com>
2024-05-04 12:59:44 +0900

    Update : DanielAI 수정

--- Assets/Scripts/Player/DanielAI.cs
+++ Assets/Scripts/Player/DanielAI.cs
@@ -12,12 +12,8 @@ public class DanielAI : MonoBehaviour
     private float playerDis; // 플레이어와 거리
     public GameObject danielRunSound; // 다니엘 발 소리
 
-    private void Update()
-    {
-        // 플레이어로부터의 방향과 거리 계산 후 추적
-        if(CalcDis()) nav.SetDestination(firstPlayer.transform.position);
-        else nav.SetDestination(transform.position);
-    }
+    // 플레이어로부터의 방향과 거리 계산 후 추적
+    private void Update() { nav.SetDestination(CalcDis() ? firstPlayer.transform.position : transform.position); }
 
     // 플레이어로부터의 방향과 거리 계산
     private bool CalcDis()
