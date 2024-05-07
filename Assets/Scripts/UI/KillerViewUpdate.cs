31e96b3bc5d4ed348c37aa88b179fc0271b8a376
gdevhun <wjh9330@naver.com>
2024-05-07 12:39:51 +0900

    FIX: OSCAR일루전 COLLIDER 살짝수정

--- Assets/Scripts/UI/KillerViewUpdate.cs
+++ Assets/Scripts/UI/KillerViewUpdate.cs
@@ -16,7 +16,7 @@ public class KillerViewUpdate : MonoBehaviour
     private void Update()
     {
         // 시간에 따라 알파값을 변경
-        float alpha = Mathf.Lerp(10f, 130f, Mathf.PingPong(Time.time * speed, 1f));
+        float alpha = Mathf.Lerp(40f, 140f, Mathf.PingPong(Time.time * speed, 1f));
         
         // 이미지의 색상을 업데이트
         _image.color = new Color(_image.color.r, _image.color.g, _image.color.b, alpha / 255f);
